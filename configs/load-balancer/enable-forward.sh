ec8d8806ced62e24af2c20199ef2bc794ca674fc
prytoegrian <prytoegrian@protonmail.com>
2019-08-11 15:40:42 +0200

    Functional statistics

--- configs/load-balancer/enable-forward.sh
+++ configs/load-balancer/enable-forward.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-iptables -t nat -A OUTPUT -p tcp --dport 8888 -j REDIRECT --to-port 8080
-iptables -t nat -A PREROUTING -p tcp --dport 8888 -j REDIRECT --to-port 8080
+iptables -t nat -A OUTPUT -p tcp --dport 8888 -j DOCKER_OUTPUT --to-port 80
+iptables -t nat -A PREROUTING -p tcp --dport 8888 -j REDIRECT --to-port 80
