b0ecb58428883a0fd0adba005b9834316599bc01
prytoegrian <prytoegrian@protonmail.com>
2019-08-11 16:14:42 +0200

    Fallback to :80

--- configs/load-balancer/disable-forward.sh
+++ configs/load-balancer/disable-forward.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-iptables -t nat -D OUTPUT -p tcp --dport 8888 -j REDIRECT --to-port 8080
-iptables -t nat -D PREROUTING -p tcp --dport 8888 -j REDIRECT --to-port 8080
+iptables -t nat -D OUTPUT -p tcp --dport 8888 -j REDIRECT --to-port 80
+iptables -t nat -D PREROUTING -p tcp --dport 8888 -j REDIRECT --to-port 80
